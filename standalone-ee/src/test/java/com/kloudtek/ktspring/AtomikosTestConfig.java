47b09daae0f42db16efde3665e7a33b2de91cc40
Yannick Menager <ymenager@kloudtek.com>
2016-09-04 10:54:27 -0700

    Implementing XA

--- standalone-ee/src/test/java/com/kloudtek/ktspring/AtomikosTestConfig.java
+++ standalone-ee/src/test/java/com/kloudtek/ktspring/AtomikosTestConfig.java
@@ -13,16 +13,13 @@ import com.kloudtek.ktspring.jms.JMSDurableTopicConfig;
 import com.kloudtek.ktspring.jms.JMSQueueConfig;
 import com.kloudtek.ktspring.jms.JMSTopicConfig;
 import com.kloudtek.ktspring.jms.JMSTopicTemplateConfig;
-import org.apache.derby.jdbc.EmbeddedXADataSource;
 import org.hsqldb.jdbc.pool.JDBCXADataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * Created by yannick on 23/8/16.
@@ -33,17 +30,18 @@ public class AtomikosTestConfig {
     @Bean(initMethod = "init", destroyMethod = "close")
     public DataSource dataSource() throws SQLException {
         AtomikosDataSourceBean datasource = new AtomikosDataSourceBean();
-//        JDBCXADataSource hsqlDs = new JDBCXADataSource();
-//        hsqlDs.setURL("jdbc:hsqldb:mem:test?hsqldb.applog=3");
-//        hsqlDs.setUser("sa");
-//        hsqlDs.setPassword("");
-        EmbeddedXADataSource xaDataSource = new EmbeddedXADataSource();
-        xaDataSource.setCreateDatabase("create");
-        xaDataSource.setDatabaseName("memory:myDB");
-        xaDataSource.setUser("sa");
-        xaDataSource.setPassword("");
-        datasource.setXaDataSource(xaDataSource);
         datasource.setUniqueResourceName("xads");
+        JDBCXADataSource hsqlDs = new JDBCXADataSource();
+        hsqlDs.setURL("jdbc:hsqldb:mem:test?hsqldb.applog=3");
+        hsqlDs.setUser("sa");
+        hsqlDs.setPassword("");
+//        EmbeddedXADataSource xaDataSource = new EmbeddedXADataSource();
+//        xaDataSource.setCreateDatabase("create");
+//        xaDataSource.setDatabaseName("memory:myDB");
+//        xaDataSource.setUser("sa");
+//        xaDataSource.setPassword("");
+        datasource.setXaDataSource(hsqlDs);
+
 //        try (Connection connection = datasource.getConnection()) {
 //            try (Statement st = connection.createStatement()) {
 //                st.execute("SET DATABASE EVENT LOG SQL LEVEL 3");
